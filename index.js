18f26cdb2cd08ad9a6a74b04f6846c6cb74e9717
blessed wesonga <blessedwesonga@gmail.com>
2024-10-10 19:43:23 +0300

    phase-0-javascript-events-event-listening-la

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    
+    const button = document.getElementById('button');
+  
+    
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    
+    button.addEventListener('click', clickAlert);
+  }
+  
+  
+  addingEventListener();
+  
